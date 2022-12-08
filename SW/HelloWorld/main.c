048369ee11fa56f6f90b133ffe46a396f7e6603f
ing-srikanth-mukku <ing-srikanth.mukku@gmx.de>
2022-12-08 19:36:02 +0100

    Hello World C first steps

---
+++ SW/HelloWorld/main.c
@@ -0,0 +1,18 @@
+/*
+ * Project Name : Hello World
+ * Descrition   : To create first C Program 
+ *                and its Build testing
+ * Author       : Ing-Siri M
+ */
+ /* Include Section */
+#include<stdio.h>
+ 
+ /* Main Function */
+int main()
+{
+   printf("Hello World");
+   return(0);
+}
+ 
+ /* End of the Code */
+ 
